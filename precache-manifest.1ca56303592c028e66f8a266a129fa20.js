5b34466995708f47474ddd0602d199bb8a1bf219
kowalus23 <kowalus23@gmail.com>
2019-07-17 18:13:49 +0200

    Updates

---
+++ precache-manifest.1ca56303592c028e66f8a266a129fa20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876a028bcbff628dfd370b74893b3d5c",
+    "url": "/basic-youtube/index.html"
+  },
+  {
+    "revision": "8e82773115a9e603146a",
+    "url": "/basic-youtube/static/css/main.533ca4b2.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/basic-youtube/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "8e82773115a9e603146a",
+    "url": "/basic-youtube/static/js/main.ca764729.chunk.js"
+  },
+  {
+    "revision": "1d2e7bba215a7354372d",
+    "url": "/basic-youtube/static/js/runtime~main.17541471.js"
+  }
+]);
